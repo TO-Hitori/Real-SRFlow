acb70a2e6ecfda870df1c68572f7a063dd9aa1ef
Hitori <100983558+TO-Hitori@users.noreply.github.com>
2024-10-08 12:38:21 +0800

    Update dataset.py
    dataset is finished

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -4,6 +4,7 @@ Writer: WJQpe
 Date: 2024 10 06 
 """
 import os
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from torch.utils.data import Dataset
 from PIL import Image
@@ -13,25 +14,29 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-
 def print_info(img):
-    print("=-"*20)
+    print("=-" * 20)
     print(type(img))
     print(img.shape)
     print(img.dtype)
     print(img.min(), img.max())
     print("=-" * 20)
 
+
 def save_numpy(img, save_path="./result/save_numpy.png"):
     img = Image.fromarray(img)
     img.save(save_path)
 
+def save_tensor(img, save_path="./result/save_numpy.png"):
+    from torchvision.utils import save_image, make_grid
+    save_image((img + 1) * 0.5, save_path)
+
 
 class LaionHRDataset(Dataset):
     def __init__(
-        self,
-        data_root: str,
-        crop_size: int = 512,
+            self,
+            data_root: str,
+            crop_size: int = 512,
 
     ):
         super(LaionHRDataset, self).__init__()
@@ -41,6 +46,46 @@ class LaionHRDataset(Dataset):
 
         self.set_module()
 
+    def __getitem__(self, item):
+        image_path = os.path.join(self.data_root, self.image_list[item])
+        image = np.array(Image.open(image_path).convert('RGB'))
+
+        image_hr = self.random_crop(image)
+        image_hr = self.image_aug(image_hr)
+
+        image_deg = self.image_degra(image_hr)
+
+        return {
+            "image_hr": self.to_tensor(image_hr),
+            "image_deg": self.to_tensor(image_deg)
+        }
+
+
+    def set_module(self):
+        self.image_crop_module = A.RandomCrop(self.crop_size, self.crop_size, p=1.0)
+        self.image_aug_module = A.OneOf([
+            A.VerticalFlip(p=0.1),
+            A.HorizontalFlip(p=0.5),
+            A.Transpose(p=0.2),
+            A.RandomRotate90(p=0.2)
+        ])
+        self.numpy2tensor_module = A.Compose([
+            A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), max_pixel_value=255, p=1.0),
+            ToTensorV2(p=1.0)
+        ])
+        '''
+        INTER_NEAREST (0): 最近邻插值。速度最快，但可能会产生块状效果。
+        INTER_LINEAR (1): 双线性插值。速度和质量之间的平衡，常用于放大图像。
+        INTER_CUBIC (2): 双三次插值。基于4x4像素邻域，生成比双线性插值更平滑的图像，但速度较慢。
+        INTER_AREA (3): 基于像素区域关系的重采样。适用于图像缩小，能产生无摩尔纹的结果。
+        INTER_LANCZOS4 (4): Lanczos插值，基于8x8像素邻域。提供最高质量，但速度最慢。
+        INTER_LINEAR_EXACT (5): 类似于双线性插值，但计算更精确。
+        INTER_NEAREST_EXACT (6): 类似于最近邻插值，但计算更精确。
+        '''
+        '''
+        First degradation processes
+        Blur -> Resize -> Noise -> JPEG
+        '''
         self.first_degra_module = A.Compose([
             A.AdvancedBlur(
                 blur_limit=(7, 21),
@@ -57,45 +102,17 @@ class LaionHRDataset(Dataset):
             ], p=1.0),
             A.OneOf([
                 A.GaussNoise(var_limit=(1, 30), p=0.5),
-                A.ISONoise(intensity=(0.05, 2), p=0.5),
+                A.ISONoise(color_shift=(0.01, 0.05), intensity=(0.05, 2), p=0.5),
             ], p=1.0),
-
-
+            A.OneOf([
+                A.ImageCompression(quality_range=(30, 95), compression_type="jpeg", p=0.5),
+                A.ImageCompression(quality_range=(30, 95), compression_type="webp", p=0.5),
+            ], p=1.0),
         ])
-
-    def __getitem__(self, item):
-        image_path = os.path.join(self.data_root, self.image_list[item])
-        image = np.array(Image.open(image_path).convert('RGB'))
-
-        image_hr = self.random_crop(image)
-        image_hr = self.image_aug(image_hr)
-
-        image_deg = self.first_degra_module(image=image_hr)["image"]
-
-        return image_hr, image_deg
-
-    def set_module(self):
         '''
-        INTER_NEAREST (0): 最近邻插值。速度最快，但可能会产生块状效果。
-        INTER_LINEAR (1): 双线性插值。速度和质量之间的平衡，常用于放大图像。
-        INTER_CUBIC (2): 双三次插值。基于4x4像素邻域，生成比双线性插值更平滑的图像，但速度较慢。
-        INTER_AREA (3): 基于像素区域关系的重采样。适用于图像缩小，能产生无摩尔纹的结果。
-        INTER_LANCZOS4 (4): Lanczos插值，基于8x8像素邻域。提供最高质量，但速度最慢。
-        INTER_LINEAR_EXACT (5): 类似于双线性插值，但计算更精确。
-        INTER_NEAREST_EXACT (6): 类似于最近邻插值，但计算更精确。
+        Second degradation processes
+        Blur -> Resize -> Noise -> JPEG + sinc
         '''
-
-        self.test_module = A.Downscale(0.5, 0.5, interpolation=cv2.INTER_LANCZOS4, always_apply=True, p=1.0)
-
-        self.image_crop_module = A.RandomCrop(self.crop_size, self.crop_size, p=1.0)
-        self.image_aug_module = A.Compose([
-            A.VerticalFlip(p=0.1),
-            A.HorizontalFlip(p=0.5),
-            A.Transpose(p=0.2),
-            A.RandomRotate90(p=0.8)
-        ])
-
-
         self.second_degra_module = A.Compose([
             A.AdvancedBlur(
                 blur_limit=(7, 21),
@@ -104,6 +121,25 @@ class LaionHRDataset(Dataset):
                 beta_limit=(0.5, 4),
                 p=0.8
             ),
+            A.OneOf([
+                A.Resize(self.crop_size // 4, self.crop_size // 4, interpolation=cv2.INTER_LINEAR_EXACT, p=0.25),
+                A.Resize(self.crop_size // 4, self.crop_size // 4, interpolation=cv2.INTER_CUBIC, p=0.25),
+                A.Resize(self.crop_size // 4, self.crop_size // 4, interpolation=cv2.INTER_AREA, p=0.25),
+                A.Resize(self.crop_size // 4, self.crop_size // 4, interpolation=cv2.INTER_LANCZOS4, p=0.25),
+            ], p=1.0),
+            A.OneOf([
+                A.GaussNoise(var_limit=(1, 25), p=0.5),
+                A.ISONoise(color_shift=(0.01, 0.05), intensity=(0.05, 1.5), p=0.5),
+            ], p=1.0),
+            A.OneOf([
+                A.ImageCompression(quality_range=(30, 95), compression_type="jpeg", p=0.5),
+                A.ImageCompression(quality_range=(30, 95), compression_type="webp", p=0.5),
+            ], p=1.0),
+            A.RingingOvershoot(
+                blur_limit=(9, 21),
+                cutoff=(np.pi / 6, np.pi / 3),
+                p=0.8
+            )
         ])
 
     def random_crop(self, img):
@@ -113,13 +149,15 @@ class LaionHRDataset(Dataset):
         return self.image_aug_module(image=img)["image"]
 
     def image_degra(self, img):
-        return self.first_degra_module(image=img)["image"]
-    def image_test(self, img):
-        return self.test_module(image=img)["image"]
+        first_degra = self.first_degra_module(image=img)["image"]
+        second_degra = self.second_degra_module(image=first_degra)["image"]
+        return second_degra
+
+    def to_tensor(self, img):
+        return self.numpy2tensor_module(image=img)["image"]
 
     def __len__(self) -> int:
         return len(self.image_list)
-
 
 from torch.utils.data import DataLoader
 
